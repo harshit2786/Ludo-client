3eaf1497c7eed05dbd1f7a03eb29ac91670d7c54
Harshit2786 <harshit.2002.singh@gmail.com>
2024-08-15 03:18:44 +0530

    start making board

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import { Button } from "@nextui-org/react"
+import { useEffect, useState } from "react";
+import { useWebHook } from "./hooks/webSocketHook"
+import { Button } from "@nextui-org/react";
 
+export const INIT_GAME = "init_game";
+export const MOVE = "move"
+export const END = "end"
+export const PENDING = "pending"
 function App() {
-
+  type Status = "REQ" | "PEN" | "CON"
+  const socket = useWebHook();
+  const [connected,setConnected] = useState<Status>("REQ")
+  useEffect(() => {
+    if(!socket){
+      return;
+    }
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if(message.type === PENDING){
+        setConnected("PEN");
+      }
+      if(message.type === INIT_GAME){
+        setConnected("CON");
+      }
+    } 
+  },[socket])
+  if(!socket){
+    return (<div className=" h-screen flex items-center justify-center w-full">Connecting...</div>)
+  }
   return (
-    <>
-      <h1 className="text-xs ">cvbnm</h1>
-      <Button >Click me</Button>
-    </>
+    <div className=" h-screen w-full flex items-center justify-center">
+      {connected==="REQ" && <Button onClick={()=> socket.send(JSON.stringify({
+        type : INIT_GAME
+      }))}>Play Game</Button>}
+      {connected==="PEN" && <div>Looking for Players to play game with....</div>}
+    </div>
   )
 }
 
