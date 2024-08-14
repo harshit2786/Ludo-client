39fd9037abc7471a8703ec255bb01845f959d017
Harshit2786 <harshit.2002.singh@gmail.com>
2024-08-15 00:41:05 +0530

    initialised react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xs ">cvbnm</h1>
+      <Button >Click me</Button>
+    </>
+  )
+}
+
+export default App
